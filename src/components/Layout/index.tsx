8bb73f64aef314293e7e21ab7b34d4a55ee6d6ad
peidrao <contatopedrorn@gmail.com>
2020-09-16 10:51:38 -0300

    adição de componentes layout e main

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Wrapper } from './styles'
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+
+
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Layout;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import GlobaStyles from './styles/GlobalStyles'
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobaStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );
