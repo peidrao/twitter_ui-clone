c36e9136da51ba5fd174794b24ec8a28c0927b64
peidrao <contatopedrorn@gmail.com>
2020-09-18 23:27:26 -0300

    MenuBar

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Main from '../Main';
+import MenuBar from '../MenuBar';
 import { Container, Wrapper } from './styles';
 
 const Layout: React.FC = () => {
   return (
     <Container>
       <Wrapper>
+        <MenuBar />
         <Main />
       </Wrapper>
     </Container>
